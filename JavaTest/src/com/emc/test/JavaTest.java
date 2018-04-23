c1479669cb3e7bef8d09de5a41d356d177bc09f4
nilaymishra99 <nilaymishra99@gmail.com>
2018-04-23 15:29:22 +0530

    Test Project
    Rahul This is a sample Project

---
+++ JavaTest/src/com/emc/test/JavaTest.java
@@ -0,0 +1,14 @@
+package com.emc.test;
+
+public class JavaTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("First Java");
+		System.out.print("First JavaTest");
+
+
+	}
+
+}
